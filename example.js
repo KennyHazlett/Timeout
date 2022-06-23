0c4790af1164f1f0c16d56c5af094e350382bd6a
Kenneth Hazlett <hazlettkenneth1@gmail.com>
2022-06-23 16:18:47 -0400

    first commit

---
+++ example.js
@@ -0,0 +1,15 @@
+var nameSpan = document.querySelector("#name");
+var foodToOrder ="Pizza";
+
+function setName(element) {
+    console.log(element.value);
+    nameSpan.innerText = element.value;
+}
+
+function pickFood(element) {
+    console.log("the food is " + element.value);
+    foodToOrder = element.value;
+}
+function order(){
+    alert("Ordering " + foodToOrder);
+}
